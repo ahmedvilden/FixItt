cc3005511c5ed420fb6e341bac01ccbba95dab21
haythamPanda <haytham.benmansour@esprit.tn>
2019-02-25 12:08:27 +0100

    haytham

--- src/LivraisonBundle/Controller/DefaultController.php
+++ src/LivraisonBundle/Controller/DefaultController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAAction()
+    public function indexAction()
     {
         return $this->render('@Livraison/Default/index.html.twig');
     }
